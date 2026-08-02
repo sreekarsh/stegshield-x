d8ed81d7435cf9d607d04ecdd099ec6e47cf0d94
Sreekarsh <sreekarsh44@gmail.com>
2026-08-02 18:57:06 +0530

    fix: import HttpModule in R2Module for R2Service

--- backend/src/storage/r2.module.ts
+++ backend/src/storage/r2.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common"
+import { HttpModule } from "@nestjs/axios"
 import { R2Service } from "./r2.service"
 
 @Module({
+  imports: [HttpModule],
   providers: [R2Service],
   exports: [R2Service],
 })
